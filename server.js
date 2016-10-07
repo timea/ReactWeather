44d914a9da2f7a9c538eab1f4839f7e643846662
Timea Kiss <k.timea.k@gmail.com>
2016-10-07 06:12:09 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express');
 
 //create our app
 var app = express();
+const PORT = process.env.PORT;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-porto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server is up and running on port 3000');
+
+app.listen(PORT , function() {
+  console.log('Express server is up and running on port ' + PORT);
 });
