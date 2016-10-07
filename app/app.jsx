bae1b36c380da83af90f37cc9632c048ea058731
Timea Kiss <k.timea.k@gmail.com>
2016-10-07 13:57:15 +0200

    Switch to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 // load CSS
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
