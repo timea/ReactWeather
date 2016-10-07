528695ce41cad415b43af82501e822b6dd8798c2
Timea Kiss <k.timea.k@gmail.com>
2016-10-07 09:01:00 +0200

    Style About page, link to resources, delete example files.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,31 @@ var React = require('react');
 var About = (props) => {
     return (
       <div>
-        <h3>About</h3>
-        <p>Welcome to the About page!</p>
+        <h1 className="text-center">About</h1>
+        <p>Thank you for checking out my weather app!</p>
+        <p>
+          Here are some of the tools I used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react/">React</a> -  JS framework
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/">Foundation</a> - FE framework
+          </li>
+          <li>
+            <a href="https://babeljs.io/">Babel</a> - transpiling
+          </li>
+          <li>
+            <a href="https://webpack.github.io/">Webpack</a> - bundling
+          </li>
+          <li>
+            <a href="https://openweathermap.org/"> Open Weather Map</a> - API called for weather information
+          </li>
+        </ul>
+        <p>You can check the code under the hood in our
+          <a href='https://github.com/timea/ReactWeather'> GitHub repo</a>
+        </p>
       </div>
     )
 };
